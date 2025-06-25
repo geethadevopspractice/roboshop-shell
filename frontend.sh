d2c4cd276c443841ae126fe47612be7c3b348d32
Geethikaakula <akulageethika6@gmail.com>
2025-06-25 18:04:43 +0530

    roboshop frontend code

--- frontend.sh
+++ frontend.sh
@@ -7,6 +7,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-# create a file
+cp nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx
 systemctl restart nginx
