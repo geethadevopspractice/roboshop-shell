155e40442ee183ad465ae0a1bf04b3dcc0bf15df
Geethikaakula <akulageethika6@gmail.com>
2025-07-01 12:19:36 +0530

    roboshop comment

--- cart.sh
+++ cart.sh
@@ -1,1 +1,18 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+cp cart.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+useradd roboshop
+mkdir /app
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
+
 
