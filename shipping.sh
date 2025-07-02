9d966598cf428948fd3b642e86a87e8ff6e0ea61
Geethikaakula <akulageethika6@gmail.com>
2025-07-02 18:17:40 +0530

    roboshop shellscripting comment

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,6 @@
   MAVEN
 
   dnf install mysql -y
-  mysql -h mysql-dev.gdevops65.shop -uroot -pRoboShop@1 < /app/db/schema.sql
-  mysql -h mysql-dev.gdevops65.shop -uroot -pRoboShop@1 < /app/db/app-user.sql
-  mysql -h mysql-dev.gdevops65.shop -uroot -pRoboShop@1 < /app/db/master-data.sql
+  mysql -h mysql-dev.gdevops65.shop -uroot -p$1 < /app/db/schema.sql
+  mysql -h mysql-dev.gdevops65.shop -uroot -p$1 < /app/db/app-user.sql
+  mysql -h mysql-dev.gdevops65.shop -uroot -p$1 < /app/db/master-data.sql
